3aa1e70775c35c5d287b754d316f133ab6ff7feb
wafloski <kontakt@mateuszkonopka.pl>
2019-09-20 23:38:40 +0200

    simple routing and added components

---
+++ src/components/ItemsList/ItemsList.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import styles from './ItemsList.module.scss';
+import cx from 'classnames';
+
+const items = [
+  '1st',
+  '2nd',
+  '3rd',
+  '4th',
+  '5th'
+];
+
+class ItemsList extends Component {
+    state = {
+        isCollapsed: false
+    };
+
+    toggle = () => {
+        this.setState(prevState => ({
+            isCollapsed: !prevState.isCollapsed
+        }));
+    }
+
+    render() {
+        const listClass = cx(styles.list, {
+            [styles.isCollapsed]: this.state.isCollapsed
+        });
+
+        return (
+            <>
+                <button className="button is-dark is-large" onClick={this.toggle}>collapse</button>
+                <ul className={listClass}>
+                    { items.map (item => (
+                        <li className="notification is-primary">{ item }</li>
+                    ))}
+                </ul>
+                <p>item is {this.state.isCollapsed.toString()}</p>
+            </>
+        );
+    }
+}
+
+export default ItemsList;
