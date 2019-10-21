1c73803aa6148f4ccfde2ed080e306bdc7e3a7cd
wafloski <kontakt@mateuszkonopka.pl>
2019-10-22 00:22:25 +0200

    compound components implementation

--- src/components/Multistep/Multistep.js
+++ src/components/Multistep/Multistep.js
@@ -1,29 +1,88 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 
 const WizardContext = React.createContext({
-   currentPage: 1,
-   changePage: () => {},
+    currentPage: 1,
+    changePage: () => {},
+    pageIndexes: [],
+    updatePageIndexes: () => {}
 });
 
+const ProgressBar = () => {
+    const { currentPage, pageIndexes } = useContext(WizardContext);
+
+    const outerWrapperStyle = {
+        width: "100%",
+        height: "20px",
+        backgroundColor: "grey"
+    }
+
+    const innerWrapperStyle = {
+        width: "100%",
+        height: "20px",
+        backgroundColor: "green",
+        transition: "transform .5s ease-out",
+        transform: `scaleX(${currentPage / pageIndexes.length})`,
+        transformOrigin: "0% 50%"
+    };
+
+    return (
+        <div style={outerWrapperStyle}>
+            <div style={innerWrapperStyle}></div>
+        </div>
+    )
+}
+
 const Page = ({children, pageIndex}) => {
-    const {currentPage} = useContext(WizardContext);
+    const {currentPage, updatePageIndexes} = useContext(WizardContext);
+
+    useEffect(() => {
+        updatePageIndexes(pageIndex);
+    })
 
     return currentPage === pageIndex ? children : null;
 };
 
 const Controls = () => {
-    const {changePage, currentPage} = useContext(WizardContext);
+    const {changePage, currentPage, pageIndexes} = useContext(WizardContext);
 
     return (
         <div>
-            <button onClick={() => changePage(currentPage - 1)}>Previous</button>
-            <button onClick={() => changePage(currentPage + 1)}>Next</button>
+            <button
+                disabled={currentPage === 1}
+                onClick={() => changePage(currentPage - 1)}
+                className="button is-primary is-small"
+            >
+                Previous
+            </button>
+            <button
+                disabled={currentPage === pageIndexes.length}
+                onClick={() => changePage(currentPage + 1)}
+                className="button is-warning is-small"
+            >
+                Next
+            </button>
+            { currentPage === pageIndexes.length &&
+                <button
+                    className="button is-warning is-danger"
+                >
+                    Submit
+                </button>
+            }
         </div>
     );
 };
 
 const Wizard = ({children}) => {
     const [currentPage, setCurrentPage] = useState(1);
+    const [pageIndexes, setPageIndexes] = useState([]);
+
+    const updatePageIndexes = pageIndex => {
+        if (pageIndexes.includes(pageIndex)) {
+            return;
+        }
+
+        setPageIndexes([...pageIndexes, pageIndex]);
+    };
 
     const changePage = newPageIndex => {
         setCurrentPage(newPageIndex);
@@ -32,11 +91,13 @@ const Wizard = ({children}) => {
     return (
         <WizardContext.Provider value={{
             currentPage,
-            changePage
+            changePage,
+            pageIndexes,
+            updatePageIndexes
         }}>
             {children}
         </WizardContext.Provider>
     )
 };
 
-export { Page, Controls, Wizard };
+export { Page, Controls, Wizard, ProgressBar };
--- src/views/Contacts.js
+++ src/views/Contacts.js
@@ -10,6 +10,7 @@ const Contacts = () => (
             <Multistep.Page pageIndex={2} >page 2</Multistep.Page>
             <Multistep.Page pageIndex={3} >page 3</Multistep.Page>
             <Multistep.Controls />
+            <Multistep.ProgressBar/>
         </Multistep.Wizard>
     </div>
 );
