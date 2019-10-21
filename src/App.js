8e2e8afd99e9d3492db870a5c823be6b29fada96
wafloski <kontakt@mateuszkonopka.pl>
2019-10-21 22:56:24 +0200

    custom hook implementation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Docs from "./views/Docs.js";
 import Tips from "./views/Tips.js";
 import Patterns from "./views/Patterns.js";
 import Components from "./views/Components.js";
+import Users from "./views/Users.js";
 import styles from "./App.module.scss";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
                 <Route path="/tips" exact component={Tips} />
                 <Route path="/patterns" exact component={Patterns} />
                 <Route path="/components" exact component={Components} />
+                <Route path="/users" exact component={Users} />
             </div>
         </Router>
     );
--- src/views/Tips.js
+++ src/views/Tips.js
@@ -1,11 +1,31 @@
-import React from "react";
-import Columns from "../components/Columns/Columns";
+import React, { useState, useRef } from "react";
+import { useDetectOutsideClick } from "../hooks/useDetectOutsideClick";
 
-const Tips = () => (
-  <div>
-    <h2 className="title is-3">Tips</h2>
-      <Columns />
-  </div>
-);
+const Tips = () => {
+    const [isTooltipOpen, setTooltipVisibility] = useState(false);
+    const tooltipRef = useRef(null);
+    useDetectOutsideClick(tooltipRef, setTooltipVisibility);
+
+    return (
+        <div>
+            <h2 className="title is-3">Tips</h2>
+            <button
+                className="button is-large is-primary"
+                onClick={() => setTooltipVisibility(!isTooltipOpen)}
+            >
+                Open tooltip
+            </button>
+            {isTooltipOpen && (
+                <div ref={tooltipRef} class="notification is-primary">
+                    Primar lorem ipsum dolor sit amet, consectetur adipiscing elit lorem
+                    ipsum dolor. <strong>Pellentesque risus mi</strong>, tempus quis
+                    placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet
+                    fringilla. Nullam gravida purus diam, et dictum efficitur. Sit amet,
+                    consectetur adipiscing elit
+                </div>
+            )}
+        </div>
+    );
+};
 
 export default Tips;
